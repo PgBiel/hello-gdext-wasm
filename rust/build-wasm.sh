69fa790c037a852330818c9287bbab598726bf11
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-06-15 21:17:13 -0300

    initial threading attempt

--- rust/build-wasm.sh
+++ rust/build-wasm.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 cd "$(dirname $0)"
-EM_CACHE=$(mktemp -d) cargo +nightly build \
+
+# Multithreaded wasm
+RUSTFLAGS="-C link-args=-pthread" EM_CACHE=$(mktemp -d) cargo +nightly build \
   --features godot/experimental-wasm,godot/lazy-function-tables \
+  -Zbuild-std --target wasm32-unknown-emscripten $@
+
+mv target/debug/coolbeans.wasm target/debug/coolbeans.threads.wasm
+
+# Single-threaded wasm
+EM_CACHE=$(mktemp -d) cargo +nightly build \
+  --features nothreads,godot/experimental-wasm,godot/lazy-function-tables \
   -Zbuild-std --target wasm32-unknown-emscripten $@
