d25bfc52c22b863cd1b8ad0e10f70711f4e16c9d
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-06-15 21:18:07 -0300

    initial fixes for web

--- rust/build-wasm.sh
+++ rust/build-wasm.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd "$(dirname $0)"
+set -e
 
 # Multithreaded wasm
-RUSTFLAGS="-C link-args=-pthread" EM_CACHE=$(mktemp -d) cargo +nightly build \
+RUSTFLAGS="-C link-args=-pthread -C link-args=-sSIDE_MODULE=2 -C target-feature=+atomics,+bulk-memory,+mutable-globals -Zlink-native-libraries=no -Cllvm-args=-enable-emscripten-cxx-exceptions=0" EM_CACHE=$(mktemp -d) cargo +nightly build \
   --features godot/experimental-wasm,godot/lazy-function-tables \
   -Zbuild-std --target wasm32-unknown-emscripten $@
 
-mv target/debug/coolbeans.wasm target/debug/coolbeans.threads.wasm
+mv target/wasm32-unknown-emscripten/debug/coolbeans.wasm target/wasm32-unknown-emscripten/debug/coolbeans.threads.wasm || true
 
 # Single-threaded wasm
 EM_CACHE=$(mktemp -d) cargo +nightly build \
