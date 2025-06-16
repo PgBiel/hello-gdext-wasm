f460f8afa114beb8f755b1099323fce76b3fec59
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-06-15 21:28:44 -0300

    remove wasm binary rename

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,4 @@ use godot::prelude::*;
 struct MyExtension;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {
-    fn override_wasm_binary() -> Option<&'static str> {
-        // Binary name unchanged (mycrate.wasm) without thread support
-        #[cfg(feature = "nothreads")]
-        return None;
-
-        // Tell gdext we add a custom suffix to the binary with thread support
-        // Please note that this is not needed if "mycrate.threads.wasm" is used
-        // (you could return None as well in that particular case)
-        #[cfg(not(feature = "nothreads"))]
-        Some("a.wasm")
-    }
-}
+unsafe impl ExtensionLibrary for MyExtension {}
