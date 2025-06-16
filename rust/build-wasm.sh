93c1af8e8fd103b63295b6e7d569885426d8b470
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-06-15 21:20:39 -0300

    basic threading

--- rust/build-wasm.sh
+++ rust/build-wasm.sh
@@ -3,7 +3,11 @@ cd "$(dirname $0)"
 set -e
 
 # Multithreaded wasm
-RUSTFLAGS="-C link-args=-pthread -C link-args=-sSIDE_MODULE=2 -C target-feature=+atomics,+bulk-memory,+mutable-globals -Zlink-native-libraries=no -Cllvm-args=-enable-emscripten-cxx-exceptions=0" EM_CACHE=$(mktemp -d) cargo +nightly build \
+RUSTFLAGS="-C link-args=-pthread \
+-C link-args=-sSIDE_MODULE=2 \
+-C target-feature=+atomics,+bulk-memory,+mutable-globals \
+-Zlink-native-libraries=no \
+-Cllvm-args=-enable-emscripten-cxx-exceptions=0" EM_CACHE=$(mktemp -d) cargo +nightly build \
   --features godot/experimental-wasm,godot/lazy-function-tables \
   -Zbuild-std --target wasm32-unknown-emscripten $@
 
