83bf063e0d1edaea81871a51301f3de3f8d50ce8
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-06-15 21:19:45 -0300

    custom wasm binary

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,5 +4,10 @@ use godot::prelude::*;
 
 struct MyExtension;
 
+#[cfg(feature = "nothreads")]
 #[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+#[cfg(not(feature = "nothreads"))]
+#[gdextension(wasm_binary = "coolbeans.threads.wasm")]
 unsafe impl ExtensionLibrary for MyExtension {}
